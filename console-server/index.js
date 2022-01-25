f1ba772ed772ed1750c72e680987097d4ef33d2c
Homemadestea58 <44537298+homemadestea58@users.noreply.github.com>
2022-01-25 05:11:23 +0000

    god damn it

--- console-server/index.js
+++ console-server/index.js
@@ -5,15 +5,10 @@ const { version } = require('../package.json');
 
 var app = http.createServer(function (req,res){
 	// Set CORS headers
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-	res.setHeader('Access-Control-Allow-Headers', req.header.origin);
-	if ( req.method === 'OPTIONS' ) {
-		res.writeHead(200);
-		res.end();
-		return;
-	}
+	res.setHeader('Access-Control-Allow-Headers', req.headers.origin);
 	return;
 	// ...
 });
