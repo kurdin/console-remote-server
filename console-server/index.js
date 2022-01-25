a95108420c3b8f4c89ef2884dc0945858b744a43
Homemadestea58 <44537298+homemadestea58@users.noreply.github.com>
2022-01-25 04:20:36 +0000

    Maybe now?

--- console-server/index.js
+++ console-server/index.js
@@ -12,6 +12,16 @@ const io = require('socket.io')(app, {
 		methods: ['GET', 'POST'],
 		allowedHeaders: ["Access-Control-Allow-Origin"],
 	},
+	handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+
 });
 
 // eslint-disable-next-line no-console
