9bfaeffdf2677dfcdc623d28a1511ee834cfe3b4
Homemadestea58 <44537298+homemadestea58@users.noreply.github.com>
2022-01-25 03:47:30 +0000

    cors?

--- console-server/index.js
+++ console-server/index.js
@@ -22,7 +22,7 @@ console.log(
 	}`
 );
 
-app.use(function (req, res, next) {
+io.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
